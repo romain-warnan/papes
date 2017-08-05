e983bc600699aed3ec0fa1875c167751768b8af2
Romain Warnan <romain.warnan@gmail.com>
2017-08-05 12:42:45 +0200

    Recherche du meilleur prénom

--- src/main/java/fr/plaisance/papes/PapesApplication.java
+++ src/main/java/fr/plaisance/papes/PapesApplication.java
@@ -1,12 +1,25 @@
 package fr.plaisance.papes;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PapesApplication {
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class PapesApplication implements CommandLineRunner {
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(PapesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		System.out.println("Recherche du meilleur prénom pour un garçon…");
+		Executors
+			.newScheduledThreadPool(1)
+			.schedule(() -> System.out.println("Pierre"), 5, TimeUnit.SECONDS)
+			.get();
 	}
 }
