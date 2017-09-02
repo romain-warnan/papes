ba7e6b5d2c557d6ceb9db97d026f8ad3bbe1b4ff
Romain Warnan <romain.warnan@gmail.com>
2017-09-02 20:22:25 +0200

    Nouvelles couleurs pour les nationnalités

--- docs/scripts/papes-par-nationalite.js
+++ docs/scripts/papes-par-nationalite.js
@@ -10,12 +10,22 @@ const draw = (json) => {
     const labels = json.map(item => item.label)
     const values = json.map(item => item.value)
 
+    const colors = {}
+    colors[columns[0][0]] = '#c55a77'
+    colors[columns[1][0]] = '#793939'
+    colors[columns[2][0]] = '#cdd5c6'
+    colors[columns[3][0]] = '#a68ba5'
+    colors[columns[4][0]] = '#094eac'
+    colors[columns[5][0]] = '#fb421d'
+    colors[columns[6][0]] = '#1b998b'
+    colors[columns[7][0]] = '#ffda8f'
+
     c3.generate({
         bindto: '#papes-par-nationalite',
         data: {
             type: 'donut',
             columns: columns,
-            // colors: colors,
+            colors: colors,
         },
         tooltip: {
             format: {
