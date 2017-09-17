763f531812dab0de0b0fa029afb9b2fa29ba5cb6
Romain Warnan <romain.warnan@gmail.com>
2017-09-17 14:42:42 +0200

    Introduction de SCSS (test avec la police Ubuntu)

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,44 @@
 const path = require('path')
 const webapp = 'src/main/webapp'
-const indexPath = path.resolve(__dirname, webapp, 'scripts', 'index.js')
+const jsPath = path.resolve(__dirname, webapp, 'scripts', 'index.js')
+const scssPath = path.resolve(__dirname, webapp, 'styles', 'papes.scss')
 const buildPath = path.resolve(__dirname, webapp)
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-	entry: [indexPath],
+	entry: [jsPath, scssPath],
 	output: {
 		path: buildPath,
 		filename: 'papes.js',
 		publicPath: webapp
 	},
 	module: {
-		rules: [
-			{
-				test: /.*\.js$/,
-				include: path.join(__dirname, webapp, 'scripts'),
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				options: {
-					presets: ['es2015']
-				}
-			}
+        loaders: [
+            {
+                test: /.*\.js$/,
+                loader: 'babel-loader',
+                include: path.join(__dirname, webapp, 'scripts'),
+                exclude: /node_modules/,
+                options: {
+                    presets: ['es2015'],
+                }
+            },
+            {
+                test: /.*\.scss$/,
+                loader: ExtractTextPlugin.extract([
+                	'css-loader',
+					'sass-loader',
+				])
+            }
 		]
 	},
 	devServer: {
 		port: 3030
-	}
+	},
+    plugins: [
+		new ExtractTextPlugin({
+            filename: 'papes.css',
+			allChunks: true,
+		}),
+	],
 }
